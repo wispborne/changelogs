ccddf802015a49d933a2d6a53cf5ded5a9ace428
David Whitman <whitmadm@gmail.com>
2017-05-07 17:44:29 +0300

    Manually run jacoco after building

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+$BUDDYBUILD_WORKSPACE/gradlew jacocoTestDebugUnitTestReport
+
+cp -r --parents $BUDDYBUILD_WORKSPACE/**/build/reports/jacoco/* .
+
 curl -s https://codecov.io/bash > .codecov
 chmod +x .codecov
 ./.codecov
